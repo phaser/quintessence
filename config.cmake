f874390feedb0e57977fd168d3976ebd5cb88794
Cristian Bidea <cristi@king.com>
2013-09-24 13:11:10 +0300

    compiling and almost linking; main got lost on the way

--- config.cmake
+++ config.cmake
@@ -9,6 +9,7 @@ set (PROJECTS_ROOT_DIR  "${CMAKE_CURRENT_LIST_DIR}/projects")
 set (PLATFORM_DEFAULT           "linux")
 set (PROJECT_TYPE_DEFAULT       "Unix Makefiles")
 
+# === TARGET
 set (CTARGET "SDLDemo-osx")
 list (APPEND TARGETS ${CTARGET})
 copy_target ("DEFAULT" ${CTARGET})
@@ -17,8 +18,12 @@ set (PLATFORM_${CTARGET}        "osx")
 set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
 set (BUILD_DIR_${CTARGET}       "SDLDemo")
 set (PROJECT_TYPE_${CTARGET}    "Xcode")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev -DRUNTIME_SUFFIX=/Debug ")
+set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DRUNTIME_SUFFIX=/Debug")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.8.sdk ")
 
+# === TARGET
 set (CTARGET "SDLDemo-win32")
 list (APPEND TARGETS ${CTARGET})
 copy_target ("DEFAULT" ${CTARGET})
