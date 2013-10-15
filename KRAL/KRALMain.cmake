c128c8907af1c4379fc46b25f12c07e0ec5ef54b
phaser <cristian.bidea@gmail.com>
2013-10-15 14:15:00 +0300

    SDL2 Framework packaged

--- KRAL/KRALMain.cmake
+++ KRAL/KRALMain.cmake
@@ -458,7 +458,7 @@ macro(link_ios_framework)
     FIND_LIBRARY (FRAMEWORK_${LIF_NAME}
                   NAMES ${LIF_NAME}
                   PATHS ${CMAKE_OSX_SYSROOT}/System/Library
-                  PATHS ${ARGV1}
+                  PATHS ${ARGV2}
                   PATH_SUFFIXES Frameworks
                   NO_DEFAULT_PATH)
     MARK_AS_ADVANCED(FRAMEWORK_${LIF_NAME})
@@ -491,7 +491,7 @@ macro(export_ios_framework)
     SET(LIF_NAME ${ARGV1})
     FIND_LIBRARY (FRAMEWORK_${LIF_NAME}
                   NAMES ${LIF_NAME}
-                  PATHS ${CMAKE_OSX_SYSROOT}/System/Library ${ARGV1}
+                  PATHS ${CMAKE_OSX_SYSROOT}/System/Library ${ARGV2}
                   PATH_SUFFIXES Frameworks
                   NO_DEFAULT_PATH)
     MARK_AS_ADVANCED(FRAMEWORK_${LIF_NAME})
