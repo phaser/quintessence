e9829c36c99a172b894320755a9a7d4623989580
Cristian Bidea <cristi@king.com>
2014-10-14 18:11:00 +0300

    link_ios_frameworks uses ARGV1 for optional custom path passing but it is wrong as ARGV1 is in use already

--- KRAL/KRALMain.cmake
+++ KRAL/KRALMain.cmake
@@ -752,7 +752,7 @@ macro(link_ios_framework)
     SET(LIF_NAME ${ARGV1})
     FIND_LIBRARY (FRAMEWORK_${LIF_NAME}
                   NAMES ${LIF_NAME}
-                  PATHS ${CMAKE_OSX_SYSROOT}/System/Library ~/Library ${ARGV1}
+                  PATHS ${CMAKE_OSX_SYSROOT}/System/Library ~/Library ${ARGV2}
                   PATH_SUFFIXES Frameworks
                   NO_DEFAULT_PATH)
     MARK_AS_ADVANCED(FRAMEWORK_${LIF_NAME})
