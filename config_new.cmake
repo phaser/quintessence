558eba8f6156c7b75ca48d1b920961a7fa26b1a7
Cristian Bidea <cristi@king.com>
2015-04-02 16:52:11 +0300

    Added more targets

--- config_new.cmake
+++ config_new.cmake
@@ -2,30 +2,43 @@ add_target (__DEFAULT
     KRAL_PATH       "${CMAKE_CURRENT_LIST_DIR}/KRAL/" 
     PACKAGE_DIRS    "${CMAKE_CURRENT_LIST_DIR}/packages;${CMAKE_CURRENT_LIST_DIR}/thirdparty"  
     PROJECTS_ROOT   "${CMAKE_CURRENT_LIST_DIR}/projects"
-    PLATFORM        "osx"
+    PLATFORM        osx
+    PROJECT_DIR     ${CMAKE_CURRENT_LIST_DIR}/SDLDemo
+    BUILD_DIR       SDLDemo
     PROJECT_TYPE    "Unix Makefiles"
+    BUILD_TYPE      debug
 )
 
 add_target (SDLDemo-qt
     INHERIT         __DEFAULT
     PLATFORM        qt5
-    PROJECT_DIR     ${CMAKE_CURRENT_LIST_DIR}/SDLDemo
-    BUILD_DIR       SDLDemo
     PROJECT_TYPE    "Unix Makefiles"
-    BUILD_TYPE      debug
     CUSTOM_ARGS     -Wdev
 )
 
 add_target (SDLDemo-osx
     INHERIT         __DEFAULT
     PLATFORM        osx
-    PROJECT_DIR     ${CMAKE_CURRENT_LIST_DIR}/SDLDemo
-    BUILD_DIR       SDLDemo
     PROJECT_TYPE    Xcode
-    BUILD_TYPE      debug
     CUSTOM_ARGS
         "-Wdev"
         " -DRUNTIME_DIR=debug"
         " -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\""
         " -DTARGETSDK=MacOSX10.9.sdk"
+)
+
+add_target (SDLDemo-web
+    INHERIT         __DEFAULT
+    PLATFORM        web
+    PROJECT_TYPE    "Unix Makefiles"
+    COMPILER_ARGS   "-Wno-warn-absolute-paths"
+    CUSTOM_ARGS     -Wdev
+    CUSTOM_ARGS_PD  "-DCMAKE_TOOLCHAIN_FILE=${KRAL_PATH}/Emscripten.cmake"
+)
+
+add_target (SDLDemo-cppcheck
+    INHERIT         __DEFAULT
+    PLATFORM        osx
+    CUSTOM_ARGS     -Wdev
+    CUSTOM_ARGS_PD  "-DCMAKE_TOOLCHAIN_FILE=${KRAL_PATH}/cppcheck.cmake"
 )
