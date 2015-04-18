bb579e0d8355e1d9c9b76fcbe1e9819ec45f4a65
phaser <cristian.bidea@gmail.com>
2015-04-18 16:12:53 +0300

    Removed old config.cmake

--- config.cmake
+++ config.cmake
@@ -1,147 +1,71 @@
-cmake_minimum_required (VERSION 2.8)
-include (KRAL/config.cmake)
+add_target (__DEFAULT
+    KRAL_PATH       "${CMAKE_CURRENT_LIST_DIR}/KRAL/" 
+    PACKAGE_DIRS    "${CMAKE_CURRENT_LIST_DIR}/packages;${CMAKE_CURRENT_LIST_DIR}/thirdparty"  
+    PROJECTS_ROOT   "${CMAKE_CURRENT_LIST_DIR}/projects"
+    PLATFORM        osx
+    PROJECT_DIR     ${CMAKE_CURRENT_LIST_DIR}/SDLDemo
+    BUILD_DIR       SDLDemo
+    PROJECT_TYPE    "Unix Makefiles"
+    BUILD_TYPE      debug
+)
 
-set (KRAL_PATH      "${CMAKE_CURRENT_LIST_DIR}/KRAL/")
-set (PACKAGE_DIRS   "${CMAKE_CURRENT_LIST_DIR}/packages;${CMAKE_CURRENT_LIST_DIR}/thirdparty")
-set (PROJECTS_ROOT_DIR  "${CMAKE_CURRENT_LIST_DIR}/projects")
+add_target (SDLDemo-qt
+    INHERIT         __DEFAULT
+    PLATFORM        qt5
+    PROJECT_TYPE    "Unix Makefiles"
+    CUSTOM_ARGS     -Wdev
+)
 
-# Default target
-set (PLATFORM_DEFAULT           "linux")
-set (PROJECT_TYPE_DEFAULT       "Unix Makefiles")
+add_target (SDLDemo-osx
+    INHERIT         __DEFAULT
+    PLATFORM        osx
+    PROJECT_TYPE    Xcode
+    CUSTOM_ARGS
+        "-Wdev"
+        " -DRUNTIME_DIR=debug"
+        " -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\""
+        " -DTARGETSDK=MacOSX10.9.sdk"
+)
 
-# === TARGET
-set (CTARGET "SDLDemo-qt")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "qt5")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Unix Makefiles")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.8.sdk ")
-set (BUILD_TYPE_${CTARGET}      "debug")
+add_target (SDLDemo-web
+    INHERIT         __DEFAULT
+    PLATFORM        web
+    PROJECT_TYPE    "Unix Makefiles"
+    COMPILER_ARGS   "-Wno-warn-absolute-paths"
+    CUSTOM_ARGS     -Wdev
+    CUSTOM_ARGS_PD  "-DCMAKE_TOOLCHAIN_FILE=${KRAL_PATH}/Emscripten.cmake"
+)
 
-# === TARGET
-set (CTARGET "SDLDemo-osx")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "osx")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Xcode")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DRUNTIME_DIR=debug")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.9.sdk ")
-#set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTESTS=True ")
-set (BUILD_TYPE_${CTARGET}      "debug")
+add_target (SDLDemo-osx-lib
+    INHERIT         __DEFAULT
+    PLATFORM        osx
+    PROJECT_TYPE    Xcode
+    CUSTOM_ARGS
+        "-Wdev"
+        " -DRUNTIME_DIR=debug"
+        " -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\""
+        " -DSHARED_LIB=1"
+)
 
-# === TARGET
-set (CTARGET "SDLDemo-web")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "web")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Unix Makefiles")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (COMPILER_CUSTOM_${CTARGET} "-Wno-warn-absolute-paths") 
-set (CUSTOM_ARGS_PD_${CTARGET}  "-DCMAKE_TOOLCHAIN_FILE=${KRAL_PATH}/Emscripten.cmake")
-set (BUILD_TYPE_${CTARGET}      "debug")
+add_target (editor-osx
+    INHERIT         __DEFAULT
+    PLATFORM        osx
+    PROJECT_TYPE    Xcode
+    GEN_CMAKELISTS  True
+    CUSTOM_ARGS
+        "-Wdev"
+        " -DRUNTIME_DIR=debug"
+        " -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\""
+        " -DTARGETSDK=MacOSX10.10.sdk"
+        " -DQT5=1"
+)
 
-# === TARGET
-set (CTARGET "SDLDemo-cppcheck")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "osx")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Unix Makefiles")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (COMPILER_CUSTOM_${CTARGET} "-Wno-warn-absolute-paths") 
-set (CUSTOM_ARGS_PD_${CTARGET}  "-DCMAKE_TOOLCHAIN_FILE=${KRAL_PATH}/cppcheck.cmake")
-set (BUILD_TYPE_${CTARGET}      "debug")
-
-# === TARGET
-set (CTARGET "SDLDemo-osx-lib")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "osx")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Xcode")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DRUNTIME_DIR=debug")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.8.sdk ")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DSHARED_LIB=1 ")
-#set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTESTS=True ")
-set (BUILD_TYPE_${CTARGET}      "debug")
-
-# === TARGET
-set (CTARGET "editor-osx")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "osx")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/editor")
-set (BUILD_DIR_${CTARGET}       "editor")
-set (PROJECT_TYPE_${CTARGET}    "Xcode")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DRUNTIME_DIR=debug")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.8.sdk ")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DQT5=1 ")
-#set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTESTS=True ")
-set (BUILD_TYPE_${CTARGET}      "debug")
-set (GENERATE_CMAKELISTS_${CTARGET} True)
-
-# == TARGET
-set (CTARGET "SDLDemo-iossim")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "ios")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Xcode")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs\"")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=iPhoneSimulator7.0.sdk ")
-set (BUILD_TYPE_${CTARGET}      "debug")
-
-# == TARGET
-set (CTARGET "SDLDemo-android")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "android")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Unix Makefiles")
-set (CMAKE_BUILD_OPTIONS_${CTARGET} " -- -j")
-set (API_LEVEL_${CTARGET} "android-14")
-set (CPU_TARGET_${CTARGET} "armeabi")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DANDROID_APP_NAME=\"SDLDemo\" ")
-set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DANDROID_APP_PACKAGE=\"ro.qui.apps\"")
-set (BUILD_TYPE_${CTARGET}      "debug")
-
-# === TARGET
-set (CTARGET "SDLDemo-win32")
-list (APPEND TARGETS ${CTARGET})
-copy_target ("DEFAULT" ${CTARGET})
-set (TARGET_NAME_${CTARGET}     "${CTARGET}")
-set (PLATFORM_${CTARGET}        "win32")
-set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/SDLDemo")
-set (BUILD_DIR_${CTARGET}       "SDLDemo")
-set (PROJECT_TYPE_${CTARGET}    "Visual Studio 11")
-set (CUSTOM_ARGS_${CTARGET}     "-Wdev -DRUNTIME_SUFFIX=/Debug/SDLDemo.app ")
-set (BUILD_TYPE_${CTARGET}      "debug")
-
+add_target (SDLDemo-iossim
+    INHERIT         __DEFAULT
+    PLATFORM        ios
+    PROJECT_TYPE    Xcode
+    CUSTOM_ARGS
+        "-Wdev"
+        " -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs\""
+        " -DTARGETSDK=iPhoneSimulator7.0.sdk "
+)
