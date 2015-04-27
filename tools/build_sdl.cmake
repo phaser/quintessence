3da6fc92befa0a0f40497a5b86f86e36c5ea1956
Cristian Bidea <cristi@king.com>
2015-04-27 18:04:00 +0300

    added build script

---
+++ tools/build_sdl.cmake
@@ -0,0 +1,4 @@
+cmake_minimum_required (VERSION 3.0)
+
+#file (DOWNLOAD "https://www.libsdl.org/release/SDL2-2.0.3.zip" "${CMAKE_SOURCE_DIR}/SDL-2.0.3.zip")
+exec_program("${CMAKE_COMMAND}" ARGS -E tar -xvf "${CMAKE_SOURCE_DIR}/SDL-2.0.3.zip")
